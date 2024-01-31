7dd2715e0824c0bb240145f359c2d057f641f76b
Endre Bernhardt <endre.bernhardt@gmail.com>
2024-01-31 11:40:21 +1000

    Swap encoder page up/down and volume control assignment

--- keyboards/splitkb/kyria/keymaps/endre-kyria/keymap.c
+++ keyboards/splitkb/kyria/keymaps/endre-kyria/keymap.c
@@ -290,26 +290,26 @@ bool oled_task_user(void) {
     return false;
 }
 #endif
+DELETE THIS LINE TO UNCOMMENT (2/2) */
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
 
     if (index == 0) {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    } else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
             tap_code(KC_PGDN);
         } else {
             tap_code(KC_PGUP);
         }
+    } else if (index == 1) {
+        // Volume control
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
     }
     return false;
 }
 #endif
-DELETE THIS LINE TO UNCOMMENT (2/2) */
